02e21b8c2d68b15ea5ae839ef2af9de4692d5b49
jiyapandit <115740240+jiyapandit@users.noreply.github.com>
2024-03-18 03:55:24 -0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using Onana_Hospital_Management_System;
-namespace Onana_Hospital_Management_System
+namespace MediNet_Hospital_Management_System
 {
     public partial class Form1 : Form
     {
